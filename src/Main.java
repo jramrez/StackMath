3389ad383ab60b2f3ee08b205df73f88ffc3f8ea
jramrez <jramrez@ucvts.org>
2024-03-19 09:20:28 -0400

    firstn

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+/* Program Name: StackMath
+ * Program Purpose: Evaluate equations using the Stack class and postfix expressions
+ * Created / Edited by: jramrez
+ */
+
+ import java.util.*;
+
+ public class Main{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Stack<Integer> stacker = new Stack<Integer>(); // declarations
+
+        System.out.println("Enter a math equation as a POSTFIX expression:");
+        String inpt = scan.nextLine(); //prompt and scan
+
+        for (int count = 0; count < inpt.length(); count++) {
+            
+        }
+        scan.close();
+    }
+}
